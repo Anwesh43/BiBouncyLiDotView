f6adb526c80c4a6ceb4651302378e7a26a26a70b
Anwesh43 <anweshthecool0@gmail.com>
2019-11-30 21:18:09 +0530

    adding constants and libraries

--- bibouncylidotview/src/main/java/com/anwesh/uiprojects/bibouncylidotview/BiBouncyLiDotView.kt
+++ bibouncylidotview/src/main/java/com/anwesh/uiprojects/bibouncylidotview/BiBouncyLiDotView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.bibouncylidotview
 /**
  * Created by anweshmishra on 30/11/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val parts : Int = 2
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val scGap : Float = 0.02f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
