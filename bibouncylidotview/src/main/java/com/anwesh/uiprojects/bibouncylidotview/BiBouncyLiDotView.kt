999843fe9c1c5c89fabeea63a6d770f8d139b2df
Anwesh43 <anweshthecool0@gmail.com>
2019-11-30 21:40:01 +0530

    adding BiBouncyLiDotView class

--- bibouncylidotview/src/main/java/com/anwesh/uiprojects/bibouncylidotview/BiBouncyLiDotView.kt
+++ bibouncylidotview/src/main/java/com/anwesh/uiprojects/bibouncylidotview/BiBouncyLiDotView.kt
@@ -55,3 +55,21 @@ fun Canvas.drawBBLDNode(i : Int, scale : Float, paint : Paint) {
     drawBiBouncyLiDot(scale, w / 2, size, paint)
     restore()
 }
+
+class BiBouncyLiDotView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
